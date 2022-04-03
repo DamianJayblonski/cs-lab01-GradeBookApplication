d0b5bfa0aef6c7b5fbb0ab9430641f966301faab
DamianJayblonski <damian.jablonski@microsoft.wsei.edu.pl>
2022-04-03 20:31:23 +0200

    Load Update

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿
 namespace GradeBook.GradeBooks
 {
-    class RankedGradeBook : BaseGradeBook
+    public class RankedGradeBook : BaseGradeBook
     {
         public RankedGradeBook(string name) : base(name)
         {
