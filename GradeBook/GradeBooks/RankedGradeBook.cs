195e6ec203ae1a84b5ea27449a5416b727311376
DamianJayblonski <damian.jablonski@microsoft.wsei.edu.pl>
2022-04-03 20:08:33 +0200

    Ranked Gradebokk done chyba nie do koncza na tablicy testow widze ale powinno byc

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -6,5 +6,9 @@ namespace GradeBook.GradeBooks
 {
     class RankedGradeBook : BaseGradeBook
     {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = Enums.GradeBookType.Ranked;
+        }
     }
 }
